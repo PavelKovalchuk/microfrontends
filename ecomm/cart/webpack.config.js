1afbcf47585e2b956c7504d903e0aeb4910dfc7e
pkovalchuk <pavel.kovalchuk@dataart.com>
2021-12-16 15:03:29 +0200

    add cart

--- ecomm/cart/webpack.config.js
+++ ecomm/cart/webpack.config.js
@@ -2,19 +2,20 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 
 module.exports = {
-  mode: "development",
+  mode: 'development',
   devServer: {
-    port: 8080,
+    port: 8082,
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: 'container',
-      remotes: {
-        products: 'products@http://localhost:8081/remoteEntry.js',
+      name: 'cart',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './CartShow': './src/index',
       },
     }),
     new HtmlWebpackPlugin({
       template: './public/index.html',
     }),
   ],
-}
+};
