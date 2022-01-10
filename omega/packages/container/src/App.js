795dc4dc7c3c838e99ed6e9459483b855e7cd9f6
pkovalchuk <pavel.kovalchuk@dataart.com>
2022-01-10 16:59:36 +0200

    trigger deploy 2

--- omega/packages/container/src/App.js
+++ omega/packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp"
 export default () => {
   return (
     <div>
-      <h1>Hi there, Pavlo!</h1>
+      <h1>Hi there, John!</h1>
       <hr />
       <MarketingApp />
     </div>
